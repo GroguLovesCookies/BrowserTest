cef05d3be98f0151d3d138ac1950679547a1fe71
GroguLovesCookies <ninad.zambare@outlook.com>
2024-01-13 14:34:00 +0100

    Added attribute selectors

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ print(selector.recursive_select(root, lambda x: selector.filter_by_element(x, "p
 print(selector.recursive_select(root, lambda x: selector.filter_by_element(x, "h1")))
 print(selector.recursive_select(root, lambda x: selector.filter_by_class(x, "emphasise")))
 print(selector.recursive_select(root, lambda x: selector.filter_by_id(x, "heading")))
-print(selector.recursive_select(root, lambda x: selector.filter_by_id(x, "second-p")))
+print(selector.recursive_select(root, lambda x: selector.filter_by_attribute_existence(x, "href")))
+print(selector.recursive_select(root, lambda x: selector.filter_by_attribute_value(x, "width", "\"100px\"")))
+print(selector.recursive_select(root, lambda x: selector.filter_by_attribute_start(x, "href", "\"../\"")))
+print(selector.recursive_select(root, lambda x: selector.filter_by_attribute_end(x, "href", "\".html\"")))
+print(selector.recursive_select(root, lambda x: selector.filter_by_attribute_content(x, "href", "\"samples\"")))
+print(selector.recursive_select(root, lambda x: selector.filter_by_attribute_pipe(x, "title", "\"test\"")))
 print(selector.filter_by_selector_chain(root, 
 [lambda x: selector.filter_by_element(x, "body"), lambda x: selector.filter_by_element(x, "p"), lambda x: selector.filter_by_element(x, "p")],
 [True, True]))
